64b88a3f5ec4325768ec0a77c06557c6421aab42
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-04-18 12:08:39 +0300

    task3

--- js/task-03.js
+++ js/task-03.js
@@ -1,3 +1,16 @@
+// 1.Напиши скрипт для создания галереи изображений по массиву данных.
+//  В HTML есть список ul.gallery.
+
+// <ul class="gallery"></ul>
+
+// 2.Используй массив объектов images для создания элементов <img> вложенных в <li>.
+//  Для создания разметки используй шаблонные строки и метод insertAdjacentHTML().
+
+// 3.Все элементы галереи должны добавляться в DOM за одну операцию вставки.
+
+// 4.Добавь минимальное оформление галереи флексбоксами или гридами через CSS классы.
+
+
 const images = [
   {
     url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
@@ -12,3 +25,18 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+console.log(images);
+
+const makeImagesMarkup = ({url, alt}) => {
+  
+  return `<li>
+      <img src = "${url}" alt = "${alt}">
+     </li>`
+};
+
+console.log (makeImagesMarkup);
+const imagesEl = document.querySelector('ul');
+const makeImages = images
+.map(makeImagesMarkup)
+.join('');
+imagesEl.insertAdjacentHTML('beforeend', makeImages);
