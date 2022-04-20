1ac7f28ca2a61e100b2bfa64620a9bd42eff9b11
NataliaPavlovskaya <91052774+NataliaPavlovskaya@users.noreply.github.com>
2022-04-20 21:07:08 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,13 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// function getRandomHexColor() {
+//   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+// }
+
+var countSheep = function (num){
+  let str = "";
+  for(let i = 1; i <= num; i++) {
+     str += `${i} sheep...`; }
+  return str;
 }
+
+console.log(countSheep);
+
